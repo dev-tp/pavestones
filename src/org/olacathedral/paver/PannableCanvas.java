93d5a27c203a1ea9692cf580a2cce93dc487c8d2
Eryk <rcksorto@yahoo.com>
2018-07-25 09:25:51 -0700

    Set fitwidth on image

--- src/org/olacathedral/paver/PannableCanvas.java
+++ src/org/olacathedral/paver/PannableCanvas.java
@@ -4,8 +4,11 @@ import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+
+import java.io.File;
 
 class PannableCanvas extends Pane {
 
@@ -26,31 +29,37 @@ class PannableCanvas extends Pane {
      * Add a grid to the canvas, send it to back
      */
     void addGrid() {
-        double w = getBoundsInLocal().getWidth();
-        double h = getBoundsInLocal().getHeight();
+//        double w = getBoundsInLocal().getWidth();
+//        double h = getBoundsInLocal().getHeight();
+//
+//        // add grid
+//        Canvas grid = new Canvas(w, h);
+//
+//        // don't catch mouse events
+//        grid.setMouseTransparent(true);
+//
+//        GraphicsContext gc = grid.getGraphicsContext2D();
+//
+//        gc.setStroke(Color.GRAY);
+//        gc.setLineWidth(1);
+//
+//        // draw grid lines
+//        double offset = 50;
+//
+//        for (double i = offset; i < w; i += offset) {
+//            gc.strokeLine(i, 0, i, h);
+//            gc.strokeLine(0, i, w, i);
+//        }
+//
+//        getChildren().add(grid);
+//
+//        grid.toBack();
 
-        // add grid
-        Canvas grid = new Canvas(w, h);
-
-        // don't catch mouse events
-        grid.setMouseTransparent(true);
-
-        GraphicsContext gc = grid.getGraphicsContext2D();
-
-        gc.setStroke(Color.GRAY);
-        gc.setLineWidth(1);
-
-        // draw grid lines
-        double offset = 50;
-
-        for (double i = offset; i < w; i += offset) {
-            gc.strokeLine(i, 0, i, h);
-            gc.strokeLine(0, i, w, i);
-        }
-
-        getChildren().add(grid);
-
-        grid.toBack();
+        String currentPath = new File(System.getProperty("user.dir")).toURI().toString();
+        ImageView background = new ImageView(currentPath + "resources/images/Motherlessboard.png");
+        background.setFitWidth(getBoundsInLocal().getWidth());
+        background.setPreserveRatio(true);
+        getChildren().add(background);
     }
 
     double getScale() {
