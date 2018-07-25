ffbc412b7a6caca82f6166e82e0763caeceed5bd
Eryk <rcksorto@yahoo.com>
2018-07-25 10:42:02 -0700

    Add circles wherever mouse points

--- src/org/olacathedral/paver/Main.java
+++ src/org/olacathedral/paver/Main.java
@@ -3,6 +3,9 @@ package org.olacathedral.paver;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 import java.io.File;
@@ -14,7 +17,17 @@ public class Main extends Application {
         String currentPath = new File(System.getProperty("user.dir")).toURI().toString();
 
         ImageView background = new ImageView(currentPath + "resources/images/Motherlessboard.png");
-        ZoomableScrollPane scrollPane = new ZoomableScrollPane(background);
+
+        Pane container = new Pane();
+        container.setPrefSize(background.getImage().getWidth(), background.getImage().getHeight());
+        container.getChildren().add(background);
+        container.setOnMouseClicked(event -> {
+            if (event.getButton() == MouseButton.PRIMARY) {
+                container.getChildren().add(new Circle(event.getX(), event.getY(), 10));
+            }
+        });
+
+        ZoomableScrollPane scrollPane = new ZoomableScrollPane(container);
 
         stage.setScene(new Scene(scrollPane, 800, 600));
         stage.show();
