e9bfc8c4046a1116ebf542ad964bf1a4e9ad6985
Eryk <rcksorto@yahoo.com>
2018-07-25 16:26:56 -0700

    Load and display coordinates from db

--- src/org/olacathedral/paver/Main.java
+++ src/org/olacathedral/paver/Main.java
@@ -5,6 +5,7 @@ import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseButton;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
@@ -16,16 +17,31 @@ public class Main extends Application {
     public void start(Stage stage) {
         String currentPath = new File(System.getProperty("user.dir")).toURI().toString();
 
-        ImageView background = new ImageView(currentPath + "resources/images/Motherlessboard.png");
+        ImageView background = new ImageView(currentPath + "resources/images/Cathedral.png");
 
         Pane container = new Pane();
         container.setPrefSize(background.getImage().getWidth(), background.getImage().getHeight());
         container.getChildren().add(background);
         container.setOnMouseClicked(event -> {
             if (event.getButton() == MouseButton.PRIMARY) {
-                container.getChildren().add(new Circle(event.getX(), event.getY(), 10));
+                Circle circle = new Circle(event.getX(), event.getY(), 1);
+                circle.setFill(Color.valueOf("#becada"));
+                container.getChildren().add(circle);
             }
         });
+
+        try {
+            Database database = new Database();
+
+            for (Integer[] coordinate : database.getAllCoordinates()) {
+                Circle circle = new Circle(coordinate[0], coordinate[1], 1);
+                circle.setFill(Color.valueOf("#becada"));
+                container.getChildren().add(circle);
+            }
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
 
         ZoomableScrollPane scrollPane = new ZoomableScrollPane(container);
 
