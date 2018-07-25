37896966bfbc99085804a5725407af66f9225f8f
Eryk <rcksorto@yahoo.com>
2018-07-25 10:24:23 -0700

    Scroll and pan map
    Checkout https://stackoverflow.com/questions/39827911/javafx-8-scaling-zooming-scrollpane-relative-to-mouse-position

--- src/org/olacathedral/paver/Main.java
+++ src/org/olacathedral/paver/Main.java
@@ -1,8 +1,26 @@
 package org.olacathedral.paver;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        String currentPath = new File(System.getProperty("user.dir")).toURI().toString();
+
+        ImageView background = new ImageView(currentPath + "resources/images/Motherlessboard.png");
+        ZoomableScrollPane scrollPane = new ZoomableScrollPane(background);
+
+        stage.setScene(new Scene(scrollPane, 800, 600));
+        stage.show();
+    }
 
     public static void main(String[] args) {
-
+        launch(args);
     }
 }
---
+++ src/org/olacathedral/paver/ZoomableScrollPane.java
@@ -0,0 +1,84 @@
+package org.olacathedral.paver;
+
+import javafx.geometry.Bounds;
+import javafx.geometry.Point2D;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+
+class ZoomableScrollPane extends ScrollPane {
+    private static final double ZOOM_RATE = 0.02;
+
+    private double scaleValue = 1.0;
+    private Node target;
+    private Node zoomNode;
+
+    ZoomableScrollPane(Node target) {
+        super();
+
+        this.target = target;
+        zoomNode = new Group(target);
+
+        setContent(outerNode(zoomNode));
+
+        setPannable(true);
+        setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        setFitToHeight(true);
+        setFitToWidth(true);
+
+        updateScale();
+    }
+
+    private Node outerNode(Node node) {
+        Node outerNode = centeredNode(node);
+
+        outerNode.setOnScroll(event -> {
+            event.consume();
+            onScroll(event.getTextDeltaY(), new Point2D(event.getX(), event.getY()));
+        });
+
+        return outerNode;
+    }
+
+    private Node centeredNode(Node node) {
+        VBox vBox = new VBox(node);
+        vBox.setAlignment(Pos.CENTER);
+        return vBox;
+    }
+
+    private void updateScale() {
+        target.setScaleX(scaleValue);
+        target.setScaleY(scaleValue);
+    }
+
+    private void onScroll(double wheelDelta, Point2D mousePoint) {
+        double zoomFactor = Math.exp(wheelDelta * ZOOM_RATE);
+
+        Bounds innerBounds = zoomNode.getLayoutBounds();
+        Bounds viewportBounds = getViewportBounds();
+
+        // calculate pixel offsets from [0, 1] range
+        double valX = getHvalue() * (innerBounds.getWidth() - viewportBounds.getWidth());
+        double valY = getVvalue() * (innerBounds.getHeight() - viewportBounds.getHeight());
+
+        scaleValue = scaleValue * zoomFactor;
+        updateScale();
+        layout(); // refresh ScrollPane scroll positions & target bounds
+
+        // convert target coordinates to zoomTarget coordinates
+        Point2D posInZoomTarget = target.parentToLocal(zoomNode.parentToLocal(mousePoint));
+
+        // calculate adjustment of scroll position (pixels)
+        Point2D adjustment = target.getLocalToParentTransform().deltaTransform(posInZoomTarget.multiply(zoomFactor - 1));
+
+        // convert back to [0, 1] range
+        // (too large/small values are automatically corrected by ScrollPane)
+        Bounds updatedInnerBounds = zoomNode.getBoundsInLocal();
+
+        setHvalue((valX + adjustment.getX()) / (updatedInnerBounds.getWidth() - viewportBounds.getWidth()));
+        setVvalue((valY + adjustment.getY()) / (updatedInnerBounds.getHeight() - viewportBounds.getHeight()));
+    }
+}
