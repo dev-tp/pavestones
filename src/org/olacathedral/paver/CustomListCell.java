c503eaff844d42082ae6c2edf2408dfe8365c477
Eryk <rcksorto@yahoo.com>
2018-07-27 13:56:39 -0700

    Add edit and delete buttons on listed items

--- src/org/olacathedral/paver/CustomListCell.java
+++ src/org/olacathedral/paver/CustomListCell.java
@@ -1,27 +1,55 @@
 package org.olacathedral.paver;
 
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
 
 public class CustomListCell extends ListCell<Donor> {
 
     private Label coordinateLabel;
     private Label donorInfoLabel;
-    private VBox container;
+    private Label editLabel;
+    private Label deleteLabel;
+    private HBox container;
 
     CustomListCell() {
         super();
 
-        container = new VBox();
-        container.setPadding(new Insets(0, 0, 0, 10));
+        container = new HBox();
+
+        VBox donorInfoContainer = new VBox();
+        donorInfoContainer.setPadding(new Insets(0, 0, 0, 10));
 
         donorInfoLabel = new Label();
         donorInfoLabel.setStyle("-fx-font-weight: bold");
         coordinateLabel = new Label();
 
-        container.getChildren().addAll(donorInfoLabel, coordinateLabel);
+        donorInfoContainer.getChildren().addAll(donorInfoLabel, coordinateLabel);
+
+        container.getChildren().add(donorInfoContainer);
+
+        Pane spacer = new Pane();
+        HBox.setHgrow(spacer, Priority.ALWAYS);
+
+        container.getChildren().add(spacer);
+
+        // Only display this in admin mode
+        editLabel = new Label("Edit");
+        deleteLabel = new Label("Delete");
+        deleteLabel.setTextFill(Paint.valueOf("#f00"));
+        deleteLabel.setPadding(new Insets(2, 0, 0, 0));
+
+        VBox editContainer = new VBox();
+        editContainer.setAlignment(Pos.CENTER_LEFT);
+        editContainer.getChildren().addAll(editLabel, deleteLabel);
+
+        container.getChildren().add(editContainer);
     }
 
     @Override
@@ -32,6 +60,8 @@ public class CustomListCell extends ListCell<Donor> {
             String alias = donor.getAlias();
             donorInfoLabel.setText(donor.getFullName() + (alias.equals("") ? "" : " (" + alias + ")"));
             coordinateLabel.setText("x: " + donor.getX() + ", y: " + donor.getY());
+            editLabel.setOnMouseClicked(event -> System.out.println("Edit: " + donor.getId()));
+            deleteLabel.setOnMouseClicked(event -> System.out.println("Delete: " + donor.getId()));
             setGraphic(container);
         }
     }
