1507740d88dd341202d88e08f50738d66f207150
Eryk <rcksorto@yahoo.com>
2018-07-30 10:01:12 -0700

    Update PaveStone columns

--- src/org/olacathedral/paver/PaveStone.java
+++ src/org/olacathedral/paver/PaveStone.java
@@ -1,19 +1,29 @@
 package org.olacathedral.paver;
 
+import java.util.Date;
+
 class PaveStone {
 
+    private Date dateSubmitted;
     private int id;
     private int x;
     private int y;
+    private String comments;
     private String dedicatedTo;
     private String donor;
 
-    PaveStone(int id, String donor, String dedicatedTo, int x, int y) {
+    PaveStone(int id, String donor, String dedicatedTo, int x, int y, String comments, Date dateSubmitted) {
+        this.comments = comments;
+        this.dateSubmitted = dateSubmitted;
         this.dedicatedTo = dedicatedTo;
         this.donor = donor;
         this.id = id;
         this.x = x;
         this.y = y;
+    }
+
+    Date getDateSubmitted() {
+        return dateSubmitted;
     }
 
     int getId() {
@@ -28,11 +38,15 @@ class PaveStone {
         return y;
     }
 
-    String getDonor() {
-        return donor;
+    String getComments() {
+        return comments;
     }
 
     String getDedicatedTo() {
         return dedicatedTo;
+    }
+
+    String getDonor() {
+        return donor;
     }
 }
