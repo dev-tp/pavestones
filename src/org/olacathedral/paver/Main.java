10d367819846964c5dc023e72771a540dd69cf8f
Eryk <rcksorto@yahoo.com>
2018-08-07 17:23:15 -0700

    Add icon

--- src/org/olacathedral/paver/Main.java
+++ src/org/olacathedral/paver/Main.java
@@ -1,7 +1,10 @@
 package org.olacathedral.paver;
 
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class Main extends Application {
 
@@ -15,6 +18,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
+        String iconsDir = new File(System.getProperty("user.dir")).toURI().toString() + "resources/icons/";
+
+        stage.getIcons().add(new Image(iconsDir + "icon.png"));
+
         LoginScene loginScene = new LoginScene(stage);
         loginScene.show();
     }
