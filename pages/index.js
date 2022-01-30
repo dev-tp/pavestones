ea133d5a8eb7105d1daad392b2e88601b832f980
Erik Sorto <rcksorto@yahoo.com>
2022-01-29 20:20:18 -0800

    Convert Java project to Next.js

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <link rel="icon" href="/icons/icon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Paver</title>
+      </Head>
+      <h1>Paver</h1>
+    </div>
+  );
+}
