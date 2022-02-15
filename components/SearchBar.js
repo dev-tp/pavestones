9dba44a72e8ae77209a736317f0fdae48e8dd696
Erik Sorto <rcksorto@yahoo.com>
2022-02-15 15:50:36 -0800

    Use a custom filter on search bar

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,13 +1,21 @@
-import Autocomplete from '@mui/material/Autocomplete';
+import Autocomplete, { createFilterOptions } from '@mui/material/Autocomplete';
 import IconButton from '@mui/material/IconButton';
 import InputBase from '@mui/material/InputBase';
 import Paper from '@mui/material/Paper';
 import React from 'react';
 import Search from '@mui/icons-material/Search';
 
+const customFilterOptions = createFilterOptions({
+  ignoreAccents: true,
+  ignoreCase: true,
+  limit: 10,
+  trim: true,
+});
+
 export default function SearchBar(props) {
   return (
     <Autocomplete
+      filterOptions={customFilterOptions}
       getOptionLabel={(option) => option.dedicated_to}
       noOptionsText="No entries"
       onChange={(_, value) => props.onSelect(value)}
