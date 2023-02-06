1a9367505d1b1eede11713f616ccb7af04e789ab
Erik Sorto <rcksorto@yahoo.com>
2023-02-06 00:45:30 -0800

    Recreate form using Tailwind

--- web/components/Form.tsx
+++ web/components/Form.tsx
@@ -1,31 +1,25 @@
-import Button from '@mui/material/Button';
-import DeleteForever from '@mui/icons-material/DeleteForever';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogTitle from '@mui/material/DialogTitle';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormGroup from '@mui/material/FormGroup';
-import IconButton from '@mui/material/IconButton';
-import MoveDown from '@mui/icons-material/MoveDown';
-import Print from '@mui/icons-material/Print';
+import { Dialog, Switch } from '@headlessui/react';
+import {
+  MapPinIcon,
+  PrinterIcon,
+  TrashIcon,
+} from '@heroicons/react/24/outline';
 import React from 'react';
-import Switch from '@mui/material/Switch';
-import TextField from '@mui/material/TextField';
 
 import FormProps from '../types/FormProps';
 import PaveStoneProps from '../types/PaveStoneProps';
 
 export default function Form(props: FormProps): JSX.Element {
   const dedicatedTo = React.createRef<HTMLInputElement>();
-  const isDeceased = React.createRef<HTMLInputElement>();
   const patron = React.createRef<HTMLInputElement>();
+
+  let isDeceased = false;
 
   function getCurrentData(): PaveStoneProps {
     return {
       ...props.data,
       dedicated_to: dedicatedTo.current?.value,
-      is_deceased: isDeceased.current?.checked,
+      is_deceased: isDeceased,
       patron: patron.current?.value,
     };
   }
@@ -39,60 +33,91 @@ export default function Form(props: FormProps): JSX.Element {
   }
 
   return (
-    <Dialog id="form" fullWidth onKeyUp={handleKeyUp} open={props.isOpen}>
-      <DialogTitle>
-        {props.data?._id && (
-          <>
-            <IconButton
-              onClick={() => props.onPositionEdit(getCurrentData())}
-              size="small"
-            >
-              <MoveDown />
-            </IconButton>
-            <IconButton onClick={print} size="small">
-              <Print />
-            </IconButton>
-            <IconButton
-              onClick={() => props.onDelete(getCurrentData())}
-              size="small"
-            >
-              <DeleteForever />
-            </IconButton>
-          </>
-        )}
-      </DialogTitle>
-      <DialogContent style={{ paddingBottom: 0 }}>
-        <TextField
-          autoFocus
-          defaultValue={props.data?.patron}
-          fullWidth
-          inputRef={patron}
-          label="Patron"
-          margin="dense"
-        />
-        <TextField
-          defaultValue={props.data?.dedicated_to}
-          fullWidth
-          inputRef={dedicatedTo}
-          label="Dedicated to"
-          margin="dense"
-        />
-        <FormGroup>
-          <FormControlLabel
-            control={
-              <Switch
-                defaultChecked={props.data?.is_deceased}
-                inputRef={isDeceased}
-              />
-            }
-            label="Deceased"
-          />
-        </FormGroup>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={props.onCancel}>Cancel</Button>
-        <Button onClick={() => props.onSave(getCurrentData())}>Save</Button>
-      </DialogActions>
+    <Dialog
+      className="relative z-20"
+      onClose={props.onCancel}
+      onKeyUp={handleKeyUp}
+      open={props.isOpen}
+    >
+      <div className="fixed inset-0 bg-black/50" />
+      <div className="fixed inset-0 overflow-y-auto">
+        <div className="flex min-h-full items-center justify-center">
+          <Dialog.Panel className="flex w-1/3 flex-col rounded-md bg-white p-4 pb-2">
+            {props.data._id && (
+              <div className="flex justify-end">
+                <button
+                  className="px-1"
+                  onClick={() => props.onPositionEdit(getCurrentData())}
+                >
+                  <MapPinIcon className="h-5 w-5" />
+                </button>
+                <button className="px-1" onClick={print}>
+                  <PrinterIcon className="h-5 w-5" />
+                </button>
+                <button
+                  className="px-1"
+                  onClick={() => props.onDelete(getCurrentData())}
+                >
+                  <TrashIcon className="h-5 w-5" />
+                </button>
+              </div>
+            )}
+            <label>Patron</label>
+            <input
+              className="mb-3 rounded-md border p-2"
+              defaultValue={props.data.patron}
+              ref={patron}
+              type="text"
+            />
+            <label>Dedicated to</label>
+            <input
+              className="mb-3 rounded-md border p-2"
+              defaultValue={props.data.dedicated_to}
+              ref={dedicatedTo}
+              type="text"
+            />
+            <Switch.Group>
+              <div className="flex items-center">
+                <Switch
+                  as={React.Fragment}
+                  defaultChecked={props.data.is_deceased}
+                  name="is-deceased"
+                  onChange={(checked) => (isDeceased = checked)}
+                >
+                  {({ checked }) => (
+                    <button
+                      className={`${
+                        checked ? 'bg-blue-500' : 'bg-gray-200'
+                      } relative inline-flex h-6 w-11 items-center rounded-full`}
+                    >
+                      <span
+                        className={`${
+                          checked ? 'translate-x-6' : 'translate-x-1'
+                        } inline-block h-4 w-4 transform rounded-full bg-white transition`}
+                      />
+                    </button>
+                  )}
+                </Switch>
+                <Switch.Label className="ml-2">Deceased</Switch.Label>
+              </div>
+            </Switch.Group>
+            <div className="flex justify-end">
+              <button
+                className="mr-2 p-2 text-sm uppercase text-gray-500"
+                onClick={props.onCancel}
+              >
+                Cancel
+              </button>
+              <button
+                className="p-2 text-sm uppercase text-blue-500"
+                onClick={() => props.onSave(getCurrentData())}
+              >
+                Save
+              </button>
+            </div>
+          </Dialog.Panel>
+        </div>
+      </div>
     </Dialog>
   );
 }
