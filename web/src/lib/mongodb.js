4825bdcc52fffe3700d487aa3eb84bfe240c4bde
Erik Sorto <rcksorto@yahoo.com>
2023-04-19 15:18:00 -0700

    Set up a dummy Mongo connection while preparing tRPC integration

--- web/src/lib/mongodb.js
+++ web/src/lib/mongodb.js
@@ -1,23 +1,17 @@
-import { MongoClient } from 'mongodb';
-
-if (!process.env.MONGODB_URI) {
-  throw new Error('Please add your Mongo URI to .env.local');
-}
-
-if (!process.env.MONGODB_DB_NAME) {
-  throw new Error('Please add your Mongo database name to .env.local');
-}
-
-let client = null;
-let database = null;
-
 export async function connection() {
-  if (client && database) {
-    return { client, database };
+  function Database() {
+    this.collection = (collection) => this;
+    this.database = () => this;
+    this.deleteOne = () => this;
+    this.find = (filters) => this;
+    this.limit = () => this;
+    this.sort = () => this;
+    this.toArray = () => [];
+    this.updateOne = () => this;
   }
 
-  client = await MongoClient.connect(process.env.MONGODB_URI);
-  database = client.db(process.env.MONGODB_DB_NAME);
-
-  return { client, database };
+  return {
+    client: {},
+    database: new Database(),
+  };
 }
