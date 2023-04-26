0f9256c09a0599debc99d13577ad79f11ed14c1c
Erik Sorto <rcksorto@yahoo.com>
2023-04-26 10:42:03 -0700

    `type`s, not `interface`s

--- src/types.ts
+++ src/types.ts
@@ -1,24 +1,24 @@
 import { Pavestone } from '@prisma/client';
 
-export interface FormProps {
+export type FormProps = {
   data: Pavestone;
   isOpen: boolean;
   onCancel: () => void;
   onDelete: (data: Pavestone) => void;
   onPositionEdit: (data: Pavestone) => void;
   onSave: (data: Pavestone) => void;
-}
+};
 
-export interface MapProps {
+export type MapProps = {
   children: JSX.Element;
   x?: number;
   y?: number;
   onClick: (event: React.SyntheticEvent<Element, MouseEvent>) => void;
-}
+};
 
-export interface MarkerProps {
+export type MarkerProps = {
   data: Pavestone;
   insertMode?: boolean;
   onClick?: () => void;
   searchResult?: boolean;
-}
+};
