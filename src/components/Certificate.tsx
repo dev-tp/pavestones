10558f5c8557c76f75ce696039820a0451a3f8a5
Erik Sorto <rcksorto@yahoo.com>
2023-05-02 16:30:02 -0700

    Render pavestone location on certificate using `canvas`

--- src/components/Certificate.tsx
+++ src/components/Certificate.tsx
@@ -1,115 +1,91 @@
 import { Pavestone } from '@prisma/client';
 import React from 'react';
 
-import { IMAGE_WIDTH, IMAGE_HEIGHT } from '../constants';
-
-const MAP_WIDTH = '7.5in';
-const MAP_HEIGHT = `calc((${IMAGE_HEIGHT} * ${MAP_WIDTH}) / ${IMAGE_WIDTH})`;
-
-const MINI_MAP_HEIGHT = 275;
-const MINI_MAP_WIDTH = 350;
-
-const label: React.CSSProperties = {
-  background: '#fff',
-  fontSize: '0.8rem',
-  left: '0.1rem',
-  padding: '0.1rem',
-  position: 'absolute',
-  top: '0.1rem',
-  zIndex: 1,
+type CertificateProps = {
+  className?: string;
+  data: Pavestone;
 };
 
-export default function Certificate({
-  data,
-}: {
-  data: Pavestone;
-}): JSX.Element {
-  function centerOnPoint(data: Pavestone) {
-    const x = `calc(-${data.x}px + ${MAP_WIDTH} * 0.5)`;
-    const y = `calc(-${data.y}px + ${MAP_HEIGHT} * 0.5)`;
+function getImage(url: string): Promise<HTMLImageElement> {
+  return new Promise((resolve, reject) => {
+    const image = new Image();
 
-    return `translate(${x}, ${y})`;
-  }
+    image.onerror = (error) => reject(error);
+    image.onload = () => resolve(image);
 
+    image.src = url;
+  });
+}
+
+function StaticMap({ className, data }: CertificateProps): JSX.Element {
+  const ref = React.useRef<HTMLCanvasElement>(null);
+
+  React.useEffect(() => {
+    (async function load() {
+      const canvas = ref.current;
+
+      if (!canvas) {
+        return;
+      }
+
+      canvas.width *= 1.5;
+      canvas.height *= 1.5;
+
+      const context = canvas.getContext('2d');
+
+      if (!context) {
+        return;
+      }
+
+      const background = await getImage('/images/cert-floor-plan.png');
+      const reference = await getImage('/images/mini-floor-plan.png');
+
+      const referenceWidth = reference.naturalWidth / 1.5;
+      const referenceHeight = reference.naturalHeight / 1.5;
+
+      const radius = 4;
+
+      context.drawImage(
+        background,
+        -data.x - radius + canvas.width / 2,
+        -data.y - radius + canvas.height / 2
+      );
+
+      context.drawImage(reference, 0, 0, referenceWidth, referenceHeight);
+
+      context.beginPath();
+      context.arc(canvas.width / 2, canvas.height / 2, radius, 0, 2 * Math.PI);
+      context.fillStyle = 'red';
+      context.fill();
+
+      context.rect(
+        (data.x / background.naturalWidth) * referenceWidth - 10,
+        (data.y / background.naturalHeight) * referenceHeight - 10,
+        20,
+        20
+      );
+      context.strokeStyle = 'red';
+      context.stroke();
+    })();
+  }, [data]);
+
+  return <canvas className={className} ref={ref} />;
+}
+
+export default function Certificate({ data }: CertificateProps): JSX.Element {
   return (
-    <div>
-      <div
-        style={{
-          display: 'flex',
-          height: '1in',
-          marginBottom: '1rem',
-          position: 'relative',
-        }}
-      >
-        <img
-          alt="Logo"
-          src="/images/logo.png"
-          style={{ height: '1in', width: '1in', position: 'absolute' }}
-        />
-        <div style={{ flexGrow: 1, textAlign: 'center' }}>
-          <p>Cathedral Pave Stone designated for:</p>
+    <div className="h-screen">
+      <img alt="logo" className="absolute h-[1in]" src="/images/logo.png" />
+      <div className="flex h-[1in] items-center justify-center">
+        <div className="text-center">
+          <p>Cathedral Pavestone designated for:</p>
           <p>{data.dedicated_to}</p>
         </div>
       </div>
-      <div
-        style={{
-          border: '1px solid',
-          boxSizing: 'border-box',
-          height: MAP_HEIGHT,
-          overflow: 'hidden',
-          position: 'relative',
-          width: '100%',
-        }}
-      >
-        <span style={label}>Enlarged area</span>
-        <div
-          style={{
-            backgroundImage: 'url(/images/cert-floor-plan.png)',
-            height: IMAGE_HEIGHT,
-            transform: centerOnPoint(data),
-            transformOrigin: '0 0',
-            width: IMAGE_WIDTH,
-          }}
-        >
-          <div
-            style={{
-              background: '#f00',
-              height: 4,
-              left: data.x,
-              position: 'absolute',
-              top: data.y,
-              width: 4,
-            }}
-          />
-        </div>
-        <div
-          style={{
-            backgroundImage: 'url(/images/mini-floor-plan.png)',
-            borderLeft: '1px solid',
-            borderTop: '1px solid',
-            bottom: 0,
-            height: MINI_MAP_HEIGHT,
-            position: 'absolute',
-            right: 0,
-            width: MINI_MAP_WIDTH,
-          }}
-        >
-          <span style={label}>Overall floor plan</span>
-          <div
-            style={{
-              border: '1px solid #f00',
-              height: 20,
-              left: `calc(${(data.x / IMAGE_WIDTH) * 100}% - 12px)`,
-              position: 'absolute',
-              top: `calc(${(data.y / IMAGE_HEIGHT) * 100}% - 25px)`,
-              width: 20,
-            }}
-          />
-        </div>
-      </div>
-      <div style={{ display: 'grid', gridTemplateColumns: '50% 50%' }}>
+      <StaticMap className="my-4 h-1/2 w-full" data={data} />
+      <div className="grid grid-cols-2 gap-8">
         <div>
-          <p>
+          <p className="mb-4">
             Thank you for your gift to the Cathedral of Our Lady of the Angels.
             Your designated paving stone is an enduring symbol of faith and
             devotion. We sincerely appreciate your generosity and invite you to
@@ -122,7 +98,7 @@ export default function Certificate({
             enlarged.
           </p>
         </div>
-        <ul style={{ columns: 2, listStyle: 'upper-latin' }}>
+        <ul className="list-[upper-latin] columns-2">
           <li>Grand Doors</li>
           <li>South Ambulatory</li>
           <li>Baptismal Font</li>
