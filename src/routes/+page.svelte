83c46cabdb459814bf8c0d739a82b49b009cc6dd
Erik Sorto <rcksorto@yahoo.com>
2026-02-02 00:01:00 -0800

    Contain CSS rules relating to `path` with `#map`
    Avoid overriding `@lucid/svelte` icons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <SearchBar class="fixed top-2 right-0 left-0 m-auto w-1/4" onselect={(id) => (selected = id)} />
-<svg width={6901} height={5139}>
+<svg id="map" width={6901} height={5139}>
 	{#each data.pavestones as pavestone}
 		<Pavestone data={pavestone} selected={pavestone.id === selected} />
 	{/each}
