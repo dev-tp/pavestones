918d30e730975730d62e72151cb21a62b15b4be7
Erik Sorto <rcksorto@yahoo.com>
2026-02-01 17:00:00 -0800

    Create `Pavestone` component to access `path.focus()`
    Using `path.focus()` allows the browser to scroll to the `selected` path

---
+++ src/lib/components/Pavestone.svelte
@@ -0,0 +1,31 @@
+<script module>
+	/** @typedef {Object} Props
+	 * @property {import('$lib/server/db/schema').Pavestone} data
+	 * @property {boolean} [selected]
+	 */
+</script>
+
+<script>
+	/** @type {Props} */
+	const { data, selected = false } = $props();
+
+	/** @type {SVGPathElement} */
+	let path;
+
+	$effect(() => {
+		if (selected) {
+			path.focus();
+		}
+	});
+</script>
+
+<path
+	bind:this={path}
+	class:selected
+	class:sold={data.patron !== ''}
+	d={data.tile}
+	onclick={() => alert(data.id)}
+	onkeydown={() => {}}
+	role="button"
+	tabindex="0"
+/>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import Pavestone from '$lib/components/Pavestone.svelte';
 	import SearchBar from '$lib/components/SearchBar.svelte';
 
 	const { data } = $props();
@@ -9,14 +10,6 @@
 <SearchBar class="fixed top-2 right-0 left-0 m-auto w-1/4" onselect={(id) => (selected = id)} />
 <svg width={6901} height={5139}>
 	{#each data.pavestones as pavestone}
-		<path
-			class:selected={pavestone.id === selected}
-			class:sold={pavestone.patron !== ''}
-			d={pavestone.tile}
-			onclick={() => alert(pavestone.id)}
-			onkeydown={() => {}}
-			role="button"
-			tabindex="0"
-		/>
+		<Pavestone data={pavestone} selected={pavestone.id === selected} />
 	{/each}
 </svg>
