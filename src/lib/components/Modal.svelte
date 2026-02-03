f026fdee577033ca4b602b925792442dcc3bb616
Erik Sorto <rcksorto@yahoo.com>
2026-02-03 00:00:00 -0800

    Make a `Modal` component

---
+++ src/lib/components/Modal.svelte
@@ -0,0 +1,35 @@
+<script module>
+	/** @typedef {Object} Props
+	 * @property {import('svelte').Snippet} [children]
+	 * @property {string} [class]
+	 * @property {boolean} open
+	 */
+</script>
+
+<script>
+	import { X } from '@lucide/svelte';
+
+	/** @type {Props} */
+	let {
+		children,
+		class: className = 'fixed inset-0 bg-black/60',
+		open = $bindable(false)
+	} = $props();
+</script>
+
+<svelte:window
+	onkeyup={(event) => {
+		if (event.key === 'Escape') {
+			open = false;
+		}
+	}}
+/>
+
+{#if open}
+	<div class={className}>
+		<button class="fixed top-4 right-4 cursor-pointer text-white" onclick={() => (open = false)}>
+			<X class="h-5 w-5" />
+		</button>
+		{@render children?.()}
+	</div>
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,12 @@
 <script>
+	import Modal from '$lib/components/Modal.svelte';
 	import Pavestone from '$lib/components/Pavestone.svelte';
 	import SearchBar from '$lib/components/SearchBar.svelte';
 
 	const { data } = $props();
+
+	/** @type {boolean} */
+	let open = $state(false);
 
 	/** @type {import('$lib/server/db/schema').Pavestone | undefined} */
 	let selected = $state();
@@ -12,12 +16,18 @@
 	class="fixed top-2 right-0 left-0 m-auto w-1/4"
 	onselect={(pavestone) => (selected = pavestone)}
 />
+
 <svg id="map" width={6901} height={5139}>
 	{#each data.pavestones as pavestone}
 		<Pavestone
 			data={pavestone}
-			onclick={() => (selected = pavestone)}
+			onclick={() => {
+				open = true;
+				selected = pavestone;
+			}}
 			selected={pavestone.id === selected?.id}
 		/>
 	{/each}
 </svg>
+
+<Modal bind:open />
