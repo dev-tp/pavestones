ef1cb85db58e2a4d790fd610d25d01c78d8a2912
Erik Sorto <rcksorto@yahoo.com>
2026-02-21 00:00:00 -0800

    Re-route to `/setup` when there are no registered user accounts

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,7 +1,20 @@
+import { redirect } from '@sveltejs/kit';
+
+import { db } from '$lib/server/db';
+import { user } from '$lib/server/db/schema';
 import sessions from '$lib/server/sessions';
 
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
+	if (event.url.pathname !== '/setup') {
+		const users = await db.select().from(user).limit(1);
+
+		if (users.length === 0) {
+			redirect(302, '/setup');
+		}
+	}
+
 	event.locals.user = await sessions.validate(event.cookies.get('session'));
+
 	return resolve(event);
 }
