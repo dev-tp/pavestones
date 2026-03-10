3e5ed2625b4fa16f959451fbc7986159feeccc3d
Erik Sorto <rcksorto@yahoo.com>
2026-03-09 17:30:04 -0700

    Display reference map on certificate

--- src/lib/components/Certificate.svelte
+++ src/lib/components/Certificate.svelte
@@ -10,6 +10,7 @@
 	import { onMount } from 'svelte';
 
 	import logo from '$lib/assets/logo.svg';
+	import referenceMap from '$lib/assets/reference-map.png';
 	import { certificate } from '$lib/storage.svelte.js';
 
 	/** @type {Props} */
@@ -61,7 +62,9 @@
 			<svg bind:this={container} class="h-full w-full">
 				<use href="#map" {x} {y} />
 			</svg>
-			<div class="absolute right-0 bottom-0 h-1/3 w-1/3 border-t border-l bg-white"></div>
+			<div class="absolute right-0 bottom-0 h-1/3 w-1/3 border-t border-l bg-white">
+				<img class="m-auto h-full" alt="Mini Floor Plan" src={referenceMap} />
+			</div>
 		</div>
 		<div class="grid grid-cols-2 gap-8">
 			<div>
