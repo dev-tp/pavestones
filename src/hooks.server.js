e3fd618fac0e732ca3747c14f19646b06d88a081
Erik Sorto <rcksorto@yahoo.com>
2026-04-03 00:30:04 -0700

    Move `/setup` redirect logic to main `+layout.server.js`

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,18 +1,7 @@
-import { redirect } from '@sveltejs/kit';
-
-import { db } from '$lib/server/db';
 import sessions from '$lib/server/sessions';
 
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
-	if (event.url.pathname !== '/setup') {
-		const user = await db.query.user.findFirst();
-
-		if (!user) {
-			redirect(302, '/setup');
-		}
-	}
-
 	event.locals.user = await sessions.validate(event.cookies.get('session'));
 
 	return resolve(event);
--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,5 +1,17 @@
+import { redirect } from '@sveltejs/kit';
+
+import { db } from '$lib/server/db';
+
 /** @type {import('./$types').LayoutServerLoad} */
 export async function load(event) {
+	if (event.url.pathname !== '/setup') {
+		const user = await db.query.user.findFirst();
+
+		if (!user) {
+			redirect(302, '/setup');
+		}
+	}
+
 	return {
 		user: event.locals.user
 	};
