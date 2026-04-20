8aa261882c9a9f34ce34aed906e7dffd07d77f1f
Erik Sorto <rcksorto@yahoo.com>
2026-04-19 23:45:00 -0700

    Replicate UI language from username on email settings page

--- src/routes/(auth)/settings/email/+page.svelte
+++ src/routes/(auth)/settings/email/+page.svelte
@@ -1,24 +1,38 @@
 <script>
+	import { enhance } from '$app/forms';
 	import { page } from '$app/state';
 	import TextInput from '$lib/components/TextInput.svelte';
+	import Toasts from '$lib/components/Toasts.svelte';
 	import validate from '$lib/validate';
 
-	const { form } = $props();
+	/** @type {string[]} */
+	let messages = $state([]);
 
-	let email = $derived(form ? form.email : page.data.user.email);
+	/** @type {string} */
+	let email = $derived(page.data.user.email);
 </script>
 
 <svelte:head>
 	<title>Pavestones - Settings</title>
 </svelte:head>
 
+<Toasts bind:messages />
+
 <form
 	class="grid gap-4"
 	method="POST"
-	onsubmit={(event) => {
+	use:enhance={({ cancel }) => {
 		if (!validate.email(email)) {
-			event.preventDefault();
+			cancel();
 		}
+
+		return async ({ result, update }) => {
+			await update({ invalidateAll: true, reset: false });
+
+			if (result.status === 200) {
+				messages.push('Email was updated!');
+			}
+		};
 	}}
 >
 	<h1 class="text-xl">Email</h1>
@@ -35,11 +49,21 @@
 		type="email"
 	/>
 	<div>
-		<button class="cursor-pointer bg-black px-3 py-1 text-white" type="submit">Update</button>
 		<button
-			class="cursor-pointer px-3 py-1 hover:underline"
-			onclick={() => (email = form ? form.email : page.data.user.email)}
-			type="button">Reset</button
+			class="cursor-pointer bg-black px-3 py-1 text-white disabled:cursor-default disabled:opacity-50"
+			disabled={email === page.data.user.email}
+			type="submit"
 		>
+			Update
+		</button>
+		{#if email !== page.data.user.email}
+			<button
+				class="cursor-pointer px-3 py-1 hover:underline"
+				onclick={() => (email = page.data.user.email)}
+				type="button"
+			>
+				Reset
+			</button>
+		{/if}
 	</div>
 </form>
